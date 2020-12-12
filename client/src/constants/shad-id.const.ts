f12c208ae842a219e8acde2dd942fb93e245c526
NickKelly1 <nick.kelly314+github@gmail.com>
2020-12-12 16:18:03 +1100

    change client docker image name

--- client/src/constants/shad-id.const.ts
+++ client/src/constants/shad-id.const.ts
@@ -2,11 +2,12 @@
 // shadow_id:
 //  - Random id in LocalStorage (& set in cookies) to carry on the users session if they don't have an account
 //  - Required to allow users to do simple things like create npms dashboards
-//  - Trivial to spoof somebodies shadow identity, so NOTHING confidential
-//    should be attached to it. Once logged in, shadow_id must be removed...
-// aliased as shad_id because "shadow_id" sounds worse
+//  - Trivial to spoof somebodies shadow identity (todo: implement hashing on server to stop spoofing...)
+
 export type _ls_shad_id_key = 'lshad_id';
 export const _ls_shad_id_key: _ls_shad_id_key = 'lshad_id';
+
+// headers can't have underscores
 
 export type _header_shad_id_key = 'shad-id';
 export const _header_shad_id_key: _header_shad_id_key = 'shad-id';
